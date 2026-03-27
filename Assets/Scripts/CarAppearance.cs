726675f54f40d382d2e052ab7bbfc8ece5036cdf
Igor Józefowicz <igorjozefowicz@gmail.com>
2026-03-27 22:35:27 +0100

    Add TextMeshPro UI, checkpoints & car appearance
    Add TextMeshPro assets and UI elements, plus checkpoint and appearance logic. Introduces CarAppearance (sets player name and car color) and CheckPointController (lap/checkpoint tracking) scripts, and updates RaceController to include a CheckPointController[] field and adjust the race countdown. Updates the Game scene to add Canvas/TextMeshPro name text, EventSystem, Cinemachine camera/brain, and attaches the new components to car GameObjects. Also adds TextMesh Pro font/material resources and updates Packages/manifest and TagManager to register required packages and tags.

---
+++ Assets/Scripts/CarAppearance.cs
@@ -0,0 +1,24 @@
+using TMPro;
+using UnityEngine;
+
+public class CarAppearance : MonoBehaviour
+{
+    public string playerName;
+    public Color carColor;
+    public TextMeshProUGUI nameText;
+    public Renderer carRenderer;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        nameText.text = playerName;
+        carRenderer.material.color = carColor;
+        nameText.color = carColor; // opcjonalne
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/RaceController.cs
+++ Assets/Scripts/RaceController.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
+using UnityEngine.UIElements.Experimental;
 
 public class RaceController : MonoBehaviour
 {
     public static bool isRacing = false;
     public static int totalLaps = 1;
     public int timer = 3;
+
+    public CheckPointController[] carControllers;
 
     void CountDown()
     {
@@ -23,11 +26,31 @@ public class RaceController : MonoBehaviour
 
     void Start()
     {
-        InvokeRepeating("CountDown", 2f, 1f);
+        InvokeRepeating("CountDown", 3, 1);
     }
 
-    void Update()
-    {
-        
-    }
+        //void Start()
+        //{
+        //    Debug.Log("-----------------------------");
+        //    InvokeRepeating("CountDown", 3, 1);
+        //    GameObject[] cars = GameObject.FindGameObjectsWithTag("Car");
+        //    carControllers = new CheckPointController[cars.Length];
+        //    for (int i = 0; i < cars.Length; i++)
+        //    {
+        //        carControllers[i] = cars[i].GetComponent<CheckPointController>();
+        //    }
+        //}
+        //void LateUpdate()
+        //{
+        //    int finishedLap = 0;
+        //    foreach (CheckPointController controller in carControllers)
+        //    {
+        //        if (controller.lap == totalLaps + 1) finishedLap++;
+        //        if (finishedLap == carControllers.Length && isRacing)
+        //        {
+        //            Debug.Log("FinishRace");
+        //            isRacing = false;
+        //        }
+        //    }
+        //}
 }
