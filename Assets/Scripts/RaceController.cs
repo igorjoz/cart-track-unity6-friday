959357d1a8d0d7c075c1eaee49a5dd20aa443299
Igor Józefowicz <igorjozefowicz@gmail.com>
2026-04-10 16:26:17 +0200

    Add start/count sounds, checkpoint prefab & UI (lesson 6 fixes)
    Add StartSound and CountSound audio assets and a new CheckPoint prefab; update Game.unity to place checkpoint prefab instances and add a Canvas with StartText UI. Modify RaceController to integrate TextMeshPro UI and an AudioSource: show/hide start text, play countdown and start audio clips, display end panel when race finishes, and add a LoadScene method. Update TextMeshPro LiberationSans material/asset serialized properties to support the UI changes.

--- Assets/Scripts/RaceController.cs
+++ Assets/Scripts/RaceController.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class RaceController : MonoBehaviour
 {
@@ -8,23 +10,43 @@ public class RaceController : MonoBehaviour
 
     public CheckPointController[] carControllers;
 
+    public TextMeshProUGUI startText;
+    AudioSource audioSource;
+    public AudioClip count;
+    public AudioClip start;
+    public GameObject endPanel;
+
     void CountDown()
     {
+        if (startText != null) startText.gameObject.SetActive(true);
+
         if (timer > 0)
         {
-            Debug.Log("Race is starting: " + timer);
+            if (startText != null) startText.text = timer.ToString();
+            if (audioSource != null && count != null) audioSource.PlayOneShot(count);
             timer--;
         }
         else
         {
-            Debug.Log("Start!");
+            if (startText != null) startText.text = "START!!!";
+            if (audioSource != null && start != null) audioSource.PlayOneShot(start);
             isRacing = true;
             CancelInvoke("CountDown");
+            Invoke("HideStartText", 1f);
         }
+    }
+
+    void HideStartText()
+    {
+        if (startText != null) startText.gameObject.SetActive(false);
     }
 
     void Start()
     {
+        audioSource = GetComponent<AudioSource>();
+        if (startText != null) startText.gameObject.SetActive(false);
+        if (endPanel != null) endPanel.SetActive(false);
+
         InvokeRepeating("CountDown", 3, 1);
 
         GameObject[] cars = GameObject.FindGameObjectsWithTag("Car");
@@ -50,9 +72,14 @@ public class RaceController : MonoBehaviour
 
             if (carsThatCompletedRace == carControllers.Length && isRacing)
             {
-                Debug.Log("Race finished!");
+                if (endPanel != null) endPanel.SetActive(true);
                 isRacing = false;
             }
         }
+    }
+
+    public void LoadScene(int index)
+    {
+        SceneManager.LoadScene(index);
     }
 }
